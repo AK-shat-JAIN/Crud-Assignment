66b6c14e784ee9a59999d6704611ea5100e0d2fa
Akshat Jain <84455686+AK-shat-JAIN@users.noreply.github.com>
2023-08-01 00:02:41 +0530

    Bugs fixed

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 const dbString = process.env.DB_STRING                           // 1. Get the database string from the .env file
 
 module.exports = dbConnect = async()=>{                          // 2. Create a function to connect to the database and export it 
-    await mongoose.connect(dbString)
+    mongoose.connect(dbString)
     .then((conn)=>{                                              // 3. Use .then() to log a message if the connection is successful
         console.log(`Database connected.. at ${conn.connection.host}`)
     }).catch((err)=>{                                            // 4. Use .catch() to log an error if the connection is unsuccessful
-        console.log(err)
+        console.log("Error while connecting to the database..", err.message)
     })
 }
 
